073f0a9792a8816e89346f1c22708159617177d3
Przemyslaw <przemek.rzymkowski@gmail.com>
2023-06-13 12:42:37 +0200

    full navbar test in PL

--- src/main/java/Utils/BaseTest.java
+++ src/main/java/Utils/BaseTest.java
@@ -2,11 +2,14 @@ package Utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.BeforeSuite;
 
-
+import java.time.Duration;
 import java.util.List;
+
 
 public class BaseTest {
     @BeforeSuite
@@ -15,15 +18,26 @@ public class BaseTest {
     }
 
     public WebElement getWebEelement(By by) {
-        try {
+        waitForElementToExist(by);
             return WebDriverProvider.getDriver().findElement(by);
-        } catch (StaleElementReferenceException e) {
-            return WebDriverProvider.getDriver().findElement(by);
-        }
+
     }
 
     public List<WebElement> getWebEelements(By by) {
         return WebDriverProvider.getDriver().findElements(by);
     }
-
+    public void waitForElementToExist(By locator) {
+        FluentWait<WebDriver> wait = new FluentWait<>(WebDriverProvider.getDriver());
+        wait.ignoring(StaleElementReferenceException.class);
+        wait.withTimeout(Duration.ofSeconds(10));
+        wait.pollingEvery(Duration.ofSeconds(1));
+        wait.until(driver -> {
+                List<WebElement> elements = getWebEelements(locator);
+                if (elements.size()>0) {
+                    return true;
+                } else {
+                    return false;
+                }
+        });
+    }
 }
--- src/test/java/Test/RanstadTest.java
+++ src/test/java/Test/RanstadTest.java
@@ -12,24 +12,33 @@ public class RanstadTest extends BaseTest {
 
 
     @Test
-    public void goToRandstad() {
+    public void randstadFirstTest() {
         WebDriverProvider.getDriver().get("https://www.randstad.pl/");
         WebElement cookieAcceptButton = getWebEelement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
         cookieAcceptButton.click();
-        firstColumnCheck();
+        navBar();
     }
+
     public void firstColumnCheck() {
         List<WebElement> list = getWebEelements(By.xpath("//ul[@class='navigation__menu navigation__menu--sub hidden--until-l']//a"));
         for (int i = 1; i <= list.size(); i++) {
-            {
-                String xpath = String.format("(//ul[@class='navigation__menu navigation__menu--sub hidden--until-l']//a)[%d]", i);
-                WebElement element = getWebEelement(By.xpath(xpath));
-                String name = element.getText();
-                element.click();
-                String url = WebDriverProvider.getDriver().getCurrentUrl();
-                System.out.println("nazwa podstrony: " + name + " " + "Url podstrony: " + url);
-                WebDriverProvider.getDriver().navigate().back();
-            }
+            String xpath = String.format("(//ul[@class='navigation__menu navigation__menu--sub hidden--until-l']//a)[%d]", i);
+            WebElement element = getWebEelement(By.xpath(xpath));
+            String name = element.getText();
+            element.click();
+            String url = WebDriverProvider.getDriver().getCurrentUrl();
+            System.out.println("nazwa podstrony: " + name + " " + "Url podstrony: " + url);
         }
     }
+    public void navBar() {
+        List<WebElement> navBarList = getWebEelements(By.xpath("//ul[@class='navigation__menu navigation__menu--main hidden--until-l']/li"));
+        for (int i = 1; i <= navBarList.size() ; i++) {
+            String xpat = String.format("(//ul[@class='navigation__menu navigation__menu--main hidden--until-l']/li/a)[%d]",i);
+            WebElement element = getWebEelement(By.xpath(xpat));
+            System.out.println(element.getText());
+            element.click();
+            firstColumnCheck();
+        }
+    }
+
 }
