f63979e0132795e82d37193004a47a6fa3e55b16
Przemyslaw <przemek.rzymkowski@gmail.com>
2023-06-20 12:29:53 +0200

    some refactor

--- src/test/java/Test/RanstadTest.java
+++ src/test/java/Test/RanstadTest.java
@@ -9,28 +9,25 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+
 public class RanstadTest extends BaseTest {
 
     private static final String FILE_PATH = "src/test/java/Test/dataFiles/RandstadPL.xlsx";
 
     @Test
-    public void randstadFirstTest() {
+    private void randstadTestPl() {
         WebDriverProvider.getDriver().get("https://www.randstad.pl/");
-        WebElement cookieAcceptButton = getWebEelement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
-        cookieAcceptButton.click();
-        navBar();
-//        System.out.println(SiteData.lista);
-        SiteData.readData(FILE_PATH);
-        for (int i = 0; i < SiteData.listOfLinks.size(); i++) {
-            String string1 = String.valueOf(SiteData.listOfLinks.get(i));
-            System.out.println(string1);
-            String string2 = String.valueOf(SiteData.lista.get(i));
-            System.out.println(string2);
-            Assert.assertEquals(string1,string2);
-        }
+        acceptCookies();
+        clickAllNavBarLinks();
+        compareLinksFromFile();
     }
 
-    public void firstColumnCheck() {
+    private void acceptCookies() {
+        WebElement cookieAcceptButton = getWebEelement(By.xpath("//button[@id='onetrust-accept-btn-handler']"));
+        cookieAcceptButton.click();
+    }
+
+    private void clickFirstColumnLinks() {
         List<WebElement> list = getWebEelements(By.xpath("//ul[@class='navigation__menu navigation__menu--sub hidden--until-l']//a"));
         for (int i = 1; i <= list.size(); i++) {
             String xpath = String.format("(//ul[@class='navigation__menu navigation__menu--sub hidden--until-l']//a)[%d]", i);
@@ -42,14 +39,24 @@ public class RanstadTest extends BaseTest {
             SiteData.lista.add(siteData);
         }
     }
-    public void navBar() {
+
+    private void clickAllNavBarLinks() {
         List<WebElement> navBarList = getWebEelements(By.xpath("//ul[@class='navigation__menu navigation__menu--main hidden--until-l']/li"));
-        for (int i = 1; i <= navBarList.size() ; i++) {
-            String xpat = String.format("(//ul[@class='navigation__menu navigation__menu--main hidden--until-l']/li/a)[%d]",i);
-            WebElement element = getWebEelement(By.xpath(xpat));
+        for (int i = 1; i <= navBarList.size(); i++) {
+            String xpath = String.format("(//ul[@class='navigation__menu navigation__menu--main hidden--until-l']/li/a)[%d]", i);
+            WebElement element = getWebEelement(By.xpath(xpath));
             System.out.println("nazwa zakładki : " + element.getText());
             element.click();
-            firstColumnCheck();
+            clickFirstColumnLinks();
+        }
+    }
+
+    private void compareLinksFromFile() {
+        SiteData.readData(FILE_PATH);
+        for (int i = 0; i < SiteData.listOfLinks.size(); i++) {
+            String string1 = String.valueOf(SiteData.listOfLinks.get(i));
+            String string2 = String.valueOf(SiteData.lista.get(i));
+            Assert.assertEquals(string1, string2);
         }
     }
 }
